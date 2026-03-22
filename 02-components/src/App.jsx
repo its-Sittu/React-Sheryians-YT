8e0b5ad21af2d60e8b953e466489a6cf412c115b
unknown <sitturaj730@gmail.com>
2026-03-22 17:46:21 +0530

    Added React components practice

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+ import Card from './components/card.jsx'
+import Navbar from './components/Navbar.jsx'
+
+
+
+// const user ='Sittu Babu';
+// const age = 21;
+// const App = () => {
+//   return (
+//   <div>  
+//      <h1>Hello , Guys I am {user}</h1>
+//      <h2>and I am {age} years old </h2>
+//   </div>
+//   )
+// }
+
+// export default App 
+
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <Navbar/> 
+        <Navbar/> 
+       <Card/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    // return 'Main HOOn Gyaaan';  
+
+    return<div className='card'>
+        <h1>Sittuaaa ree</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nostrum dolor sunt tempore cupiditate ipsam itaque, magnam laboriosam natus debitis illum culpa assumenda ut corrupti. Architecto accusantium aliquam vero dolor.</p>
+    </div>  
+}
+
+export default Card;
