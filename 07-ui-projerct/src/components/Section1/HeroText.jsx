ef22580c40f17b4e74c485206335dbf9de2cfea6
unknown <sitturaj730@gmail.com>
2026-04-24 11:00:00 +0530

    fix streak for 24 april

--- 07-ui-projerct/src/components/Section1/HeroText.jsx
+++ 07-ui-projerct/src/components/Section1/HeroText.jsx
@@ -4,7 +4,7 @@ const HeroText = () => {
   return (
       <div className='p-6'>
         <h3 className='mb-6 text-5xl font-bold leading-[1.1]'>  Prospective <br /> <span
-        className='text-gray-600  '> customer </span> <br /> Segmentation</h3>
+        className='text-blue-500 '> customer </span> <br /> Segmentation</h3>
         <p className='text-xl font-medium text-gray-600'>Discover the power of customer segmentation with our cutting-edge tool. Unleash insights, tailor strategies, and drive growth by understanding your audience like never before.</p>
 
       </div>
